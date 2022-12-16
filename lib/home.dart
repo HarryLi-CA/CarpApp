2531e0d2af4e4d2be617a381df72dcfc74d52b5d
HarryTheCoder-L <69062529+HarryTheCoder-L@users.noreply.github.com>
2022-12-16 18:19:35 -0500

    setup

---
+++ lib/home.dart
@@ -0,0 +1,49 @@
+import 'package:carpto/screens/home/homepage.dart';
+import 'package:carpto/screens/post/postpage.dart';
+import 'package:carpto/screens/profile/profilepage.dart';
+import 'package:carpto/screens/search/searchpage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          body: TabBarView(
+            children: [
+              new HomePage(),
+              new SearchPage(),
+              new PostPage(),
+              new ProfilePage(),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.person_search),
+              ),
+              Tab(
+                icon: new Icon(Icons.add_to_photos_rounded),
+              ),
+              Tab(
+                icon: new Icon(Icons.account_circle_rounded),
+              ),
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.white,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.yellow,
+          ),
+          backgroundColor: Colors.grey[900],
+        ),
+      );
+  }
+}
+              //person search add photos rounded account circle
---
+++ lib/screens/search/searchpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
